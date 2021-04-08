275963f7f2e5d40b9a33db7e0baac246b608d009
rodrigogus94 <rodrigogus@live.com>
2021-04-08 16:40:38 -0300

    Porjeto aspNet MVC

--- Startup.cs
+++ Startup.cs
@@ -23,8 +23,9 @@ namespace WebMySQL
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            string stringConexao = "Server=localhost;DataBase=TBA_DESTINATION;Uid=root;Pwd=@Lisson300494";
+        {   
+            //Conexão com o banco de dados
+            string stringConexao = "Server=localhost;DataBase=TBA_DESTINATION;Uid=;Pwd=";
             services.AddDbContext<Contexto>(options =>
             options.UseMySQL(stringConexao));
 
