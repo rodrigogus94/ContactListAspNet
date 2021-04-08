57d8f3548a8fc73896e633ca61e70914e0cdb388
rodrigogus94 <rodrigogus@live.com>
2021-04-08 16:33:26 -0300

    Porjeto aspNet MVC

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebMySQL.Models;
+
+
+namespace WebMySQL.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly Contexto _context;
+
+        public UsuariosController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // Listar todos os usuários cadastrados
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Usuario.ToListAsync());
+        }
+
+        // Listar um usuário espescífico
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuario
+
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        // Cadastro de um novo Usuário
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,DTN_ID,Nome,Celular,Email")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(usuario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
+        // Editar um usuário
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuario.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return View(usuario);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,DTN_ID,Nome,Celular,Email")] Usuario usuario)
+        {
+            if (id != usuario.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(usuario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UsuarioExists(usuario.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuario);
+        }
+
+        // Deletar um Usuário
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = await _context.Usuario
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var usuario = await _context.Usuario.FindAsync(id);
+            _context.Usuario.Remove(usuario);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UsuarioExists(int id)
+        {
+            return _context.Usuario.Any(e => e.Id == id);
+        }
+
+        public ActionResult Lista()
+        {
+            Contexto db = _context;
+            var listUsuarios = db.Usuario.ToList();
+            return View(listUsuarios);
+        }
+
+
+        [HttpPost]
+        public FileResult Exportar()
+        {
+            Contexto db = _context;
+
+            //Obtem uma lista de objetos 
+            List<object> listUsuarios = (from usuario in db.Usuario.ToList().Take(_context.Usuario.Count())
+                                         select new[] {usuario.DTN_ID, usuario.Nome, usuario.Celular, usuario.Email
+                }).ToList<object>();
+
+            //Insere o nome das colunas
+            listUsuarios.Insert(0, new string[4] { "DTN_ID", "Nome", "Celular", "Email" });
+
+            StringBuilder sb = new StringBuilder();
+
+            //Percere os funcionarios e gera o CSV
+            for (int i = 0; i < listUsuarios.Count; i++)
+            {
+                string[] usuario = (string[])listUsuarios[i];
+                for (int j = 0; j < usuario.Length; j++)
+                {
+                    //Anexa dados com separador
+                    sb.Append(usuario[j] + ",");
+                }
+                //Anexa uma nova linha
+                sb.Append("\r\n");
+            }
+            return File(Encoding.UTF8.GetBytes(sb.ToString()), "text/csv", "ListaUsuario.csv");
+        }
+
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMySQL.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Display(Name = "Id")]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "DTN_ID")]
+        [Required(ErrorMessage = "O DTN_ID é obrigatório e tem que ser de 0 à 1000.")]
+        [Range(0,1000)]
+        [Column("DTN_ID")]
+        [StringLength(10)]
+        public string DTN_ID { get; set; }
+
+        [Display(Name = "DTN_DESTINATION")]
+    
+        [Column("DTN_DESTINATION")]
+        [StringLength(10)]
+        public string DTN_DESTINATION { get; set; }
+        
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O nome obrigatório.")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", ErrorMessage =
+            "Números e caracteres especiais não são permitidos no nome.")]
+        [Column("Nome")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Celular")]
+        [Required(ErrorMessage = "O celular é obrigatório.")]
+        [RegularExpression(@"^\(?([0-9]{2})\)?[-. ]?([0-9]{5})[-. ]?([0-9]{4})$", 
+            ErrorMessage = "O formato do núemro do celular tem que ser (xx)xxxxx-xxxx.")]
+        [Column("Celular")]
+        public string Celular { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "O Email é obrigatório.")]
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage = "Informe um email válido...")]
+        [Column("Email")]
+        public string Email { get; set; }
+
+
+        
+    }
+    
+    
+}
